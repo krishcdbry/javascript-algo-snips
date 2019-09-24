ee4c71278717f2eb2d8130f0855253a422149c58
Krishcdbry <mohana.krishna@misport.com>
2019-09-24 17:32:16 +0530

    Findmagicindex update

--- cdci/recursion_dp/find_magic_index.js
+++ cdci/recursion_dp/find_magic_index.js
@@ -9,9 +9,8 @@ const findMagicIndex = (arr) => {
     let right = arr.length-1;
     
     while (left <= right) {
-        if (arr[left] == left) return left;
-  
-        const pivot = left+right >> 1;
+      if (arr[left] == left) return left;
+      const pivot = left+right >> 1;
       if (arr[pivot] == pivot) return pivot;
       else if (arr[pivot] < pivot) left = pivot+1;
       else right = pivot-1;
