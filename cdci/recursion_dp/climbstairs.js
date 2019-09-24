28fcafd778158edc07d9d45dc46997a6d44755a5
Krishcdbry <mohana.krishna@misport.com>
2019-09-24 17:28:18 +0530

    climbStairs let, const update

--- cdci/recursion_dp/climbstairs.js
+++ cdci/recursion_dp/climbstairs.js
@@ -10,7 +10,7 @@
  * @return {number}
  */
 const climbStairs = (n) => {
-    let obj = {1:1, 2:2};
+    const obj = {1:1, 2:2};
     for (let i = 3; i <= n; i++) {
         obj[i] = 0;
         if (obj.hasOwnProperty(i-1)) obj[i] += obj[i-1];
